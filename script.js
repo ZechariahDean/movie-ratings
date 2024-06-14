ad27adb44eb743c2bf11ba42a2adc3334e4c2e7f
zenithalegend <zeby11@ymail.com>
2024-06-13 23:07:22 -0400

    last minute additions

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const dog = 'stella';
 $(document).ready(() => {
-	$("#movie-submit").on("click", function(event) {
+	$("#movie-submit").on("click", (event) => {
 		event.preventDefault();
 
 		if ($("#movie-name").val().length < 2 || $("#movie-rate").val() >= 10 || $("#movie-rate").val() <= 0) return;
 
-		$(`<li class="movie-item">Title: [${$("#movie-name").val()}] Rating: [${$("#movie-rate").val()}]</li>`).appendTo('#movie-list');
+		$(`<li class="movie-item">
+		Title: [${$("#movie-name").val()}] Rating: [${$("#movie-rate").val()}]
+		<button class="movie-remove">REMOVE</button>
+		</li>`).appendTo('#movie-list');
 	});
+
+	$("#movie-list").on("click", (event) => {
+		event.preventDefault();
+		if ($(event.target).attr('class') === "movie-remove")
+			$(event.target.parentElement).remove();
+	})
 });
